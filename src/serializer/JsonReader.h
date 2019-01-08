c22e0b2ba3136bd4ea29afd918dec44d3d776a52
Faern Elghinn <daniel.gig@gmail.com>
2019-01-08 12:50:14 -0500

    Add JsonSerializer with test

--- src/serializer/JsonReader.h
+++ src/serializer/JsonReader.h
@@ -28,7 +28,7 @@ namespace jsonSerializer {
 enum DecodingFlags {
    ALLOW_NULL          = 1 << 0, /// If set, null value will NOT throw an error.
    REJECT_DUPLICATE    = 1 << 1, /// If set, an error will be thrown if a map index appear multiple time within the map.
-   IGNORE_COMMENT      = 1 << 2, /// If set, c/c+++ like comments will be ignored.
+   // IGNORE_COMMENT      = 1 << 2, /// If set, c/c+++ like comments will be ignored. // TODO: re-enable.
    ALLOW_COMMA_ERR     = 1 << 3, /// If set, I will do my best to ignore pesky comma error (missing comma at the end of a line, trailing comma at the end of a list/array, double commas).
 };
 typedef FlagSet DecodingOption;
@@ -37,74 +37,39 @@ struct Position {
    size_t line = 0;
    size_t col = 0;
    void throwErr(const std::string& err) const;
+   void update(char c);
 };
 struct JsonReader {
 public:
    DecodingOption flags;
    std::istream& iStr;
    Position pos;
-   std::stringstream buffer;
+   std::string buffer;
 
    typedef std::function<std::optional<elladan::json::Json> (JsonReader&)> parse;
    static std::vector<parse> readType;
-
-   JsonReader(std::istream& in, DecodingOption flag);
-   void throwErr(const std::string& err) const;
-
+   
    struct ErrTracker {
    public:
       ErrTracker(const std::string& err, JsonReader& reader);
-      char peekNext(bool skipWs = true);        // The next char.
-      char getNext(bool skipWs = true);         // Consume char.
+      char peekNext(bool skipWs = true); // The next char.
+      char getNext(bool skipWs = true); // Consume char.
       std::string peekWord(); // The next word.
-      void consumeWord();     // Consume last word
+      std::string getWord(); // Consume last word
+      std::string get(int count); // Get the next 'count' characters.
 
       JsonReader& reader;
       const std::string&  error;
-   private:
-      inline void fillBuffer();
    };
 
-private:
+   JsonReader(std::istream& in, DecodingOption flag);
+   void throwErr(const std::string& err) const;
+   ErrTracker operator()(std::string errMessage = "");
+   
    std::optional<elladan::json::Json> extractNext();
-}
+};
 
-
-
-
-
-
-
-
-
-
-
-
-   // struct ErrTracker {
-   // public:
-   //    ErrTracker(const std::string& err, bool skipWs, bool skipCom, JsonReader& str);
-   //    int operator >>(char& cur);
-   //    char peek();
-
-   //    std::string _err;
-   //    JsonReader& _str;
-   //    bool _skip_ws;
-   //    bool _skip_comment;
-   // };
-
-   // JsonReader(std::istream* in, DecodingOption flag);
-   // void throwErr(const std::string& err) const;
-   // ErrTracker operator()(std::string err = "", bool skipWs = true, bool skipComment = true);
-   // inline void pushBack(char c);
-   // int operator >>(char& c);
-   // char peek(bool skipWs = true);
-   // inline void consume();
-
-   // std::string jsonToString();
-   // std::optional<Json> extractNext();
-
-
-Json read(std::istream* in, DecodingOption flag = DecodingOption());
-std::vector<Json> extract(std::istream* in, DecodingOption flag, const std::string& path);
+json::Json read(std::istream& in, DecodingOption flag = DecodingOption());
+std::vector<json::Json> extract(std::istream& in, DecodingOption flag, const std::string& path);
 
 }}} // namespaces
--- test/JsonDeserializer.cpp
+++ test/JsonDeserializer.cpp
@@ -1,286 +1,147 @@
-// /*
-//  * JsonSerializertest.cpp
-//  *
-//  *  Created on: Jun 13, 2017
-//  *      Author: daniel
-//  */
+/*
+ * JsonSerializertest.cpp
+ *
+ *  Created on: Jun 13, 2017
+ *      Author: daniel
+ */
 
-// #include <elladan/Stringify.h>
-// #include <elladan/UUID.h>
-// #include <bitset>
-// #include <exception>
-// #include <memory>
-// #include <sstream>
-// #include <vector>
+#include <elladan/Stringify.h>
+#include <elladan/UUID.h>
+#include <elladan/Binary.h>
+#include <bitset>
+#include <exception>
+#include <memory>
+#include <sstream>
+#include <vector>
 
-// #include "../src/json.h"
-// #include "../src/serializer/JsonReader.h"
-// #include "Test.h"
+#include "../src/json.h"
+#include "../src/serializer/JsonReader.h"
+#include "Test.h"
 
-// using std::to_string;
-// using namespace elladan::json::jsonSerializer;
+using std::to_string;
+using namespace elladan::json::jsonSerializer;
 
-// static const std::string ExpectWS =
-//         "{\n"
-//             "\t\"arr\" : [\n"
-//             "\t],\n"
-//             "\t\"obj\" : {\n"
-//             "\t},\n"
-//             "\t\"string\" : \"testing how this work\",\n"
-//             "\t\"int\" : 0,\n"
-//             "\t\"fill_arr\" : [\n"
-//                 "\t\t0,\n"
-//                 "\t\t1,\n"
-//                 "\t\t2,\n"
-//                 "\t\t3\n"
-//             "\t],\n"
-//             "\t\"bool\" : true,\n"
-//             "\t\"double\" : 3.141500,\n"
-//             "\t\"null\" : null\n"
-//         "}";
+static const std::string ExpectWS =
+        "{\n"
+            "\t\"arr\" : [\n"
+            "\t],\n"
+            "\t\"obj\" : {\n"
+            "\t},\n"
+            "\t\"string\" : \"testing how this work\",\n"
+            "\t\"int\" : 0,\n"
+            "\t\"fill_arr\" : [\n"
+                "\t\t0,\n"
+                "\t\t1,\n"
+                "\t\t2,\n"
+                "\t\t3\n"
+            "\t],\n"
+            "\t\"bool\" : true,\n"
+            "\t\"double\" : 3.141500,\n"
+            "\t\"null\" : null\n"
+        "}";
 
-// static std::string test_throw_exep(const std::string& txt){
-//     std::string retVal;
-//     std::stringstream ss;
-//     ss << txt;
+static std::string test_throw_exep(const std::string& txt, DecodingOption opt = DecodingOption()) {
+    std::string retVal;
+    std::stringstream ss;
+    ss << txt;
 
-//     try{
-//         Json obj = jsonSerializer::read(&ss);
-//         retVal += "\nDecoding \"" + txt + "\" should have thrown an exception";
-//     }  catch (std::exception& e) {    }
-//     return retVal;
-// }
+    try{
+        Json obj = jsonSerializer::read(ss, opt);
+        retVal += "\nDecoding \"" + txt + "\" should have thrown an exception.";
+    }  catch (std::exception& e) {    }
+    return retVal;
+}
 
-// #define test_mode_value_noFormat(txt, type, opt)  \
-//     try{ \
-//         std::stringstream ss;\
-//         ss << txt; \
-//         Json obj = jsonSerializer::read(&ss, opt); \
-//         if (!obj.isOfType<type>()) \
-//             retVal += "\nCould not decode \"" txt "\" as " #type; \
-//     } \
-//     catch (std::exception& e) { \
-//         retVal += std::string("\nCould not decode \"" txt "\" as " #type " ") + e.what(); \
-//     }
+template<typename T>
+std::string test_mode_value_noFormat(std::string txt, DecodingOption opt = DecodingOption()) {
+   try{ 
+      std::stringstream ss;
+      ss << txt; 
+      Json obj = jsonSerializer::read(ss, opt); 
+      if (!obj.isOfType<T>()) 
+         return std::string("\nCould not decode \"") +  txt  + "\" as " + typeid(T).name(); 
+   } 
+   catch (std::exception& e) { 
+      return std::string("\nCould not decode \"") + txt + "\" as " + typeid(T).name() + " " + e.what(); 
+   }
+	return "";
+}
 
-// #define test_mode_value(txt, type, expected) \
-//     try{ \
-//         std::stringstream ss;\
-//         ss << txt; \
-//         Json obj = jsonSerializer::read(&ss); \
-//         if (!obj.isOfType<type>()) \
-//             retVal += "\nCould not decode \"" txt "\" as " #type; \
-//         else if (obj.as<type>() != expected) { \
-//             retVal += "\nInvalid value for \"" txt "\", expected "; \
-//             retVal += to_string(expected) + " got " + to_string(obj.as<type>()); \
-//         }\
-//     }\
-//     catch (std::exception& e) { \
-//         retVal += "\nCould not decode \"" txt "\" as " #type; \
-//     }
+template<typename T>
+std::string test_mode_value(std::string txt, T expected, DecodingOption opt = DecodingOption()) {
+   try{
+      std::stringstream ss;
+      ss << txt;
+      Json obj = jsonSerializer::read(ss, opt);
+      if (!obj.isOfType<T>())
+         return std::string("\nCould not decode \"") + txt + "\" as "+ typeid(T).name();
+      else if (obj.as<T>() != expected) {
+         return std::string("\nInvalid value for \"") + txt + "\", expected " + to_string(expected) + " got " + to_string(obj.as<T>());
+      }
+   }
+   catch (std::exception& e) {
+      return std::string("\nCould not decode \"") + txt + "\" as " + typeid(T).name() + " :" + e.what();
+   }
+	return "";
+}
 
-// std::string testTxtToJson(){
-//     std::string retVal;
+std::string testTxtToJson(){
+   std::string retVal;
+   retVal += test_throw_exep("null");
+   retVal += test_mode_value_noFormat<Null>("null", DecodingFlags::ALLOW_NULL);
 
-//     // test_throw_exep("null");
-//     // test_mode_value_noFormat("null", Null, DecodingFlags::ALLOW_NULL);
+   retVal += test_mode_value<bool>("true", true);
+   retVal += test_mode_value<bool>("false", false);
 
-//     test_mode_value ("true", bool, true)
-//     test_mode_value ("false", bool, false)
+   retVal += test_mode_value<int64_t>("2", 2);
+   retVal += test_mode_value<int64_t>("-100", -100);
+   retVal += test_mode_value<int64_t>("07", 7);
 
-//     // test_mode_value ("2", int64_t, 2)
-//     // test_mode_value ("-100", int64_t, -100)
-//     // test_mode_value ("07", int64_t, 7)
-//     // test_throw_exep("100s");
+   retVal += test_mode_value<double>("7.", 7.);
+   retVal += test_mode_value<double>("0.07", 0.07);
+   retVal += test_mode_value<double>("-200.34", -200.34);
+   retVal += test_mode_value<double>("-200.34e-17", -200.34e-17);
 
-//     // test_mode_value ("7.", double, 7.)
-//     // test_mode_value ("0.07", double, 0.07)
-//     // test_mode_value ("-200.34", double, -200.34)
-//     // test_mode_value ("-200.34e-17", double, -200.34e-17)
-//     // test_throw_exep("-20e0.34e-17");
+   retVal += test_mode_value<std::string>("\"Some text with \\\"\"", "Some text with \"");
 
-//     // test_mode_value ("\"Some text with \\\"\"", std::string, "Some text with \"")
+   retVal += test_mode_value<Array>("[1,2,3]", {1,2,3});
+   retVal += test_mode_value<Array>("[ 1,\t2, 3 ]", {1,2,3});
+   retVal += test_throw_exep("[,1,,2 3,]");
+   retVal += test_mode_value<Array>("[,1,,2 3,]", {1,2,3}, DecodingFlags::ALLOW_COMMA_ERR);
 
+	Object t1;	t1.insert("key1", 1);	t1.insert("key2", 3.1415);
+   retVal += test_mode_value<Object>("{\"key1\":1,\"key2\":3.1415}", t1);
+   retVal += test_mode_value<Object>("{,,\"key1\"1 \"key2\" :\t3.1415,,}", t1, DecodingFlags::ALLOW_COMMA_ERR);
+   retVal += test_throw_exep("{\"key1\":1,\"key1\":2}", DecodingFlags::REJECT_DUPLICATE);
 
-//     // Json obj;
+   // retVal += test_mode_value<Object>("/* Test de comm\n" 
+	// "taire\n" 
+	// "*/\n"
+	// "{\n" 
+	// "\"key1\":1,//Cmoment\n"
+	// "\"key2\":3.1415 //Plus\n"
+	// "// Even more comments\n"
+	// "}//endWithComments", t1, DecodingFlags::IGNORE_COMMENT);
 
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "[1,2,3]";
-//     //     obj = jsonSerializer::read(&ss, DecodingOption());
-//     //     if (obj->getType() != JSON_ARRAY)
-//     //         retVal += "\nCould not decode \"[1,2,3]\" as JSON_ARRAY";
-//     //     else if (std::dynamic_pointer_cast<JsonArray>(obj)->value.size() != 3)
-//     //         retVal += "\nInvalid number of value in array for \"[1,2,3]\"";
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \"[1,2,3]\" as JSON_ARRAY ";
-//     //     retVal += e.what();
-//     // }
+	Object t2;
+	t2.emplace("arr", Array());
+	t2.emplace("obj", Object());
+	t2.emplace("string", "testing how this work");
+	t2.emplace("int", 0);
+	t2.emplace("fill_arr", Array {0,1,2,3});
+	t2.emplace("bool", true);
+	t2.emplace("double", 3.1415);
+	t2.emplace("null", Null());
+   retVal += test_mode_value<Object>(ExpectWS, t2, DecodingFlags::ALLOW_NULL);
 
-//     // // Test white space
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << " [  1,\t2 , 3 ]  \t";
-//     //     obj = jsonSerializer::read(&ss, DecodingOption());
-//     //     if (obj->getType() != JSON_ARRAY)
-//     //         retVal += "\nCould not decode \" [  1,\t2 , 3 ]  \t\" as JSON_ARRAY";
-//     //     else if (std::dynamic_pointer_cast<JsonArray>(obj)->value.size() != 3)
-//     //         retVal += "\nInvalid number of value in array for \" [  1,\t2 , 3 ]  \t\"";
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \" [  1,\t2 , 3 ]  \t\" as JSON_ARRAY ";
-//     //     retVal += e.what();
-//     // }
+	retVal += test_mode_value<UUID>("u\"5d79e19a-317c-4d42-8299-08b5432db803\"", elladan::UUID::fromString("5d79e19a-317c-4d42-8299-08b5432db803"));
+	retVal += test_mode_value<Binary>("b\"01234567\"", elladan::Binary("01234567"));
 
-//     // // Test comma no error.
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "[1,2 3,]";
-//     //     obj = jsonSerializer::read(&ss, DF_ALLOW_COMMA_ERR);
-//     //     if (obj->getType() != JSON_ARRAY)
-//     //         retVal += "\nCould not decode \"[1,2 3,]\" as JSON_ARRAY";
-//     //     else if (std::dynamic_pointer_cast<JsonArray>(obj)->value.size() != 3)
-//     //         retVal += "\nInvalid number of value in array for \"[1,2 3,]\"";
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \"[1,2 3,]\" as JSON_ARRAY ";
-//     //     retVal += e.what();
-//     // }
-
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "{\"key1\":1,\"key2\":3.1415}";
-//     //     obj = jsonSerializer::read(&ss, DecodingOption());
-//     //     if (obj->getType() != JSON_OBJECT)
-//     //         retVal += "\nCould not decode \"{\"key1\":1}\" as JSON_OBJECT";
-//     //     else {
-//     //         JsonObject_t tt = std::dynamic_pointer_cast<JsonObject>(obj);
-//     //         if (tt->value.size() != 2)
-//     //             retVal += "\nInvalid number of value in object for \"{\"key1\":1,\"key2\":3.1415}\"";
-//     //         else if (tt->value.find("key1") == tt->value.end())
-//     //             retVal += "\nCould not find key in \"{\"key1\":1,\"key2\":3.1415}\"";
-//     //         else if (tt->value.find("key1")->second->getType() != JSON_INTEGER)
-//     //             retVal += "\nInvalid 1st value type in \"{\"key1\":1,\"key2\":3.1415}\"";
-//     //         else if (tt->value.find("key2") == tt->value.end())
-//     //             retVal += "\nCould not find key in \"{\"key1\":1,\"key2\":3.1415}\"";
-//     //         else if (tt->value.find("key2")->second->getType() != JSON_DOUBLE)
-//     //             retVal += "\nInvalid 2nd value type in \"{\"key1\":1,\"key2\":3.1415}\"";
-//     //     }
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \"{\"key1\":1}\" as JSON_OBJECT ";
-//     //     retVal += e.what();
-//     // }
-
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "{  \"key1\"1 \"key2\" :\t3.1415,}";
-//     //     obj = jsonSerializer::read(&ss, DF_ALLOW_COMMA_ERR);
-//     //     if (obj->getType() != JSON_OBJECT)
-//     //         retVal += "\nCould not decode \"{\"key1\":1}\" as JSON_OBJECT";
-//     //     else {
-//     //         JsonObject_t tt = std::dynamic_pointer_cast<JsonObject>(obj);
-//     //         if (tt->value.size() != 2)
-//     //             retVal += "\nInvalid number of value in object for \"{  \"key1\"1 \"key2\" :\t3.1415,}\"";
-//     //         else if (tt->value.find("key1") == tt->value.end())
-//     //             retVal += "\nCould not find key in \"{  \"key1\"1 \"key2\" :\t3.1415,}\"";
-//     //         else if (tt->value.find("key1")->second->getType() != JSON_INTEGER)
-//     //             retVal += "\nInvalid value type in \"{  \"key1\"1 \"key2\" :\t3.1415,}\"";
-//     //         else if (tt->value.find("key2") == tt->value.end())
-//     //             retVal += "\nCould not find key in \"{  \"key1\"1 \"key2\" :\t3.1415,}\"";
-//     //         else if (tt->value.find("key2")->second->getType() != JSON_DOUBLE)
-//     //             retVal += "\nInvalid value type in \"{  \"key1\"1 \"key2\" :\t3.1415,}\"";
-//     //     }
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \"{  \"key1\"1 \"key2\" :\t3.1415,}\" as JSON_OBJECT ";
-//     //     retVal += e.what();
-//     // }
-
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "{  \"key1\":1, \"key1\":3.1415}";
-//     //     obj = jsonSerializer::read(&ss, DF_REJECT_DUPLICATE);
-//     //     retVal += "\nShould have refused duplicate key";
-//     // }
-//     // catch (std::exception& e) {    }
-
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "/* Test de commentai\n"
-//     //             "re\n"
-//     //             "*/\n"
-//     //             "{ \n "
-//     //             "  // Besoin de stock\n"
-//     //             "  \"key1\":1//Plus\n"
-//     //             "  ,\"key2\":2//Plus\n"
-//     //             ", \n"
-//     //             "  // plus de commentaire\n"
-//     //             "}// Et on fini avec desc commentaire";
-//     //     obj = jsonSerializer::read(&ss, DF_IGNORE_COMMENT);
-//     //     if (obj->getType() != JSON_OBJECT)
-//     //         retVal += "\nCould not decode comment as JSON_OBJECT";
-//     //     else {
-//     //         JsonObject_t tt = std::dynamic_pointer_cast<JsonObject>(obj);
-//     //         if (tt->value.size() != 1)
-//     //             retVal += "\nInvalid number of value in object for comment";
-//     //         else if (tt->value.find("key1") == tt->value.end())
-//     //             retVal += "\nCould not find key in comment";
-//     //         else if (tt->value.find("key1")->second->getType() != JSON_INTEGER)
-//     //             retVal += "\nInvalid value type in comment";
-//     //     }
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode comments as JSON_OBJECT ";
-//     //     retVal += e.what();
-//     // }
-
-//     // // Touty Frutty.
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << ExpectWS;
-//     //     obj = jsonSerializer::read(&ss, DecodingFlags::DF_ALLOW_NULL);
-
-//     //     if (obj->getType() != JSON_OBJECT)
-//     //         retVal += "\nCould not decode \"" + ExpectWS + "\" as JSON_OBJECT";
-//     //     else {
-//     //         JsonObject* tt = std::dynamic_pointer_cast<JsonObject>(obj).get();
-//     //         if (tt->value.size() != 8)
-//     //             retVal += "\nInvalid number of element reading " + ExpectWS + "";
-//     //     }
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \""+ ExpectWS +"\" as JSON_OBJECT ";
-//     //     retVal += e.what();
-//     // }
-
-//     // // Binary.
-//     // try{
-//     //     std::stringstream ss;
-//     //     ss << "\"FFFFFFFF\"";
-//     //     obj = jsonSerializer::read(&ss, DecodingOption());
-
-//     //     if (obj->getType() != JSON_STRING)
-//     //         retVal += "\nCould not decode \"FFFFFFFF\" as JSON_STRING";
-//     //     else {
-//     //         Binary_t bin = fromJson<Binary_t>(obj);
-//     //         if (bin->size != 4)
-//     //             retVal += "\nInvalid sine reading \"FFFFFFFF\"";
-//     //         else if (*((uint32_t*)bin->data) != (uint32_t) -1)
-//     //             retVal += "\nInvalid data reading \"FFFFFFFF\"";
-//     //     }
-//     // }
-//     // catch (std::exception& e) {
-//     //     retVal += "\nCould not decode \"FFFFFFFF\" as JSON_BINARY ";
-//     //     retVal += e.what();
-//     // }
-
-//     return retVal;
-// }
+   return retVal;
+}
 
 int main(int argc, char **argv) {
 	bool valid = true;
-// 	EXE_TEST(testTxtToJson());
+	EXE_TEST(testTxtToJson());
 	return valid ? 0 : -1;
 }
